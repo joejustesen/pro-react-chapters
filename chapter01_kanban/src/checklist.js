728ded1c10b2373b46ff9af12901e9c67ddf0286
Joe Justesen <jcjustesen@gmail.com>
2016-02-06 15:17:38 -0500

    Added event handler for hiding/showing card details

--- chapter01_kanban/src/checklist.js
+++ chapter01_kanban/src/checklist.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class CheckList extends Component {
     render() {
-        let tasks = this.props.tasks.map((task) => (
+        const tasks = this.props.tasks.map((task) => (
             <li className="checklist__task">
                 <input type="checkbox" defaultChecked={task.done} />
                 {task.name}
