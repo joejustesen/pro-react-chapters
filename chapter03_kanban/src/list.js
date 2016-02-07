2d197fa6522a23cddb2d8f00b0c910c31f6413fb
Joe Justesen <jcjustesen@gmail.com>
2016-02-07 12:15:54 -0500

    Added PropType checks with custom type check.

---
+++ chapter03_kanban/src/list.js
@@ -0,0 +1,25 @@
+import React, { Component, PropTypes } from 'react';
+import Card from './card';
+
+class List extends Component {
+    render() {
+        const cards = this.props.cards.map((card) => {
+            return <Card key={card.id} {...card} />
+
+        });
+
+        return (
+            <div className="list">
+                <h1>{this.props.title}</h1>
+                {cards}
+            </div>
+            );
+    }
+};
+
+List.propTypes = {
+    title: PropTypes.string.isRequired,
+    cards: PropTypes.arrayOf(PropTypes.object)
+};
+
+export default List;
